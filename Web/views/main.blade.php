b6bdb4f3d640add867cd6a9ead404372304f6d4b
ririblathmerhen <mongjja106@gmail.com>
2019-07-10 21:30:34 +0900

    게시판 디자인

--- Web/views/main.blade.php
+++ Web/views/main.blade.php
@@ -1,10 +1,159 @@
 @extends('layout/master')
 
 @section('maincontent')
+    <!-- 사이트 환영 로고 -->
     <div class="main-logo">
         <div class="logo-img"></div>
         <section id="logo-img-text">
 
         </section>
     </div>
+
+    <section id="reviews">
+        <div class="top-review">
+            <div class="review-head">
+                <span>인기 리뷰</span>
+            </div>
+
+            <div class="review-box">
+                <ul>
+                    <li class="top-review">
+                        <div class="review-thumbnail">
+                            <img src="images/2.jpg" alt="썸네일">
+                        </div>
+                        <div class="review-content">
+                            <div class="review-title">
+                                <h2>이것은 리뷰다!</h2>
+                            </div>
+                            <div class="review-in-content">
+                                <p>대충 리뷰를 열심히 쓰고있는 글대충 리뷰를 열심히 쓰고있는 글대충 리뷰를 열심히 쓰고있는 글대충 리뷰를 열심히 쓰고있는 글대충 리뷰를 열심히 쓰고있는 글대충 리뷰를 열심히 쓰고있는 글대충 리뷰를 열심히 쓰고있는 글대충 리뷰를 열심히 쓰고있는 글대충 리뷰를 열심히 쓰고있는 글대충 리뷰를 열심히 쓰고있는 글대충 리뷰를 열심히 쓰고있는 글대충 리뷰를 열심히 쓰고있는 글대충 리뷰를 열심히 쓰고있는 글대충 리뷰를 열심히 쓰고있는 글대충 리뷰를 열심히 쓰고있는 글</p>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="top-review">
+                        <div class="review-thumbnail">
+                            <img src="images/6.bmp" alt="썸네일">
+                        </div>
+                        <div class="review-content">
+                            <div class="review-title">
+                                <h2>이것은 리뷰다!</h2>
+                            </div>
+                            <div class="review-in-content">
+                                <p>대충 리뷰를 열심히 쓰고있는 글대충 리뷰를 열심히 쓰고있는 글대충 리뷰를 열심히 쓰고있는 글대충 리뷰를 열심히 쓰고있는 글대충 리뷰를 열심히 쓰고있는 글대충 리뷰를 열심히 쓰고있는 글대충 리뷰를 열심히 쓰고있는 글대충 리뷰를 열심히 쓰고있는 글대충 리뷰를 열심히 쓰고있는 글대충 리뷰를 열심히 쓰고있는 글대충 리뷰를 열심히 쓰고있는 글대충 리뷰를 열심히 쓰고있는 글대충 리뷰를 열심히 쓰고있는 글대충 리뷰를 열심히 쓰고있는 글대충 리뷰를 열심히 쓰고있는 글</p>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="top-review">
+                        <div class="review-thumbnail">
+                            <img src="images/5.jpg" alt="썸네일">
+                        </div>
+                        <div class="review-content">
+                            <div class="review-title">
+                                <h2>이것은 리뷰다!</h2>
+                            </div>
+                            <div class="review-in-content">
+                                <p>대충 리뷰를 열심히 쓰고있는 글대충 리뷰를 열심히 쓰고있는 글대충 리뷰를 열심히 쓰고있는 글대충 리뷰를 열심히 쓰고있는 글대충 리뷰를 열심히 쓰고있는 글대충 리뷰를 열심히 쓰고있는 글대충 리뷰를 열심히 쓰고있는 글대충 리뷰를 열심히 쓰고있는 글대충 리뷰를 열심히 쓰고있는 글대충 리뷰를 열심히 쓰고있는 글대충 리뷰를 열심히 쓰고있는 글대충 리뷰를 열심히 쓰고있는 글대충 리뷰를 열심히 쓰고있는 글대충 리뷰를 열심히 쓰고있는 글대충 리뷰를 열심히 쓰고있는 글</p>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="top-review">
+                        <div class="review-thumbnail">
+                            <img src="images/4.jpg" alt="썸네일">
+                        </div>
+                        <div class="review-content">
+                            <div class="review-title">
+                                <h2>이것은 리뷰다!</h2>
+                            </div>
+                            <div class="review-in-content">
+                                <p>대충 리뷰를 열심히 쓰고있는 글대충 리뷰를 열심히 쓰고있는 글대충 리뷰를 열심히 쓰고있는 글대충 리뷰를 열심히 쓰고있는 글대충 리뷰를 열심히 쓰고있는 글대충 리뷰를 열심히 쓰고있는 글대충 리뷰를 열심히 쓰고있는 글대충 리뷰를 열심히 쓰고있는 글대충 리뷰를 열심히 쓰고있는 글대충 리뷰를 열심히 쓰고있는 글대충 리뷰를 열심히 쓰고있는 글대충 리뷰를 열심히 쓰고있는 글대충 리뷰를 열심히 쓰고있는 글대충 리뷰를 열심히 쓰고있는 글대충 리뷰를 열심히 쓰고있는 글</p>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="top-review">
+                        <div class="review-thumbnail">
+                            <img src="images/6.bmp" alt="썸네일">
+                        </div>
+                        <div class="review-content">
+                            <div class="review-title">
+                                <h2>이것은 리뷰다!</h2>
+                            </div>
+                            <div class="review-in-content">
+                                <p>대충 리뷰를 열심히 쓰고있는 글대충 리뷰를 열심히 쓰고있는 글대충 리뷰를 열심히 쓰고있는 글대충 리뷰를 열심히 쓰고있는 글대충 리뷰를 열심히 쓰고있는 글대충 리뷰를 열심히 쓰고있는 글대충 리뷰를 열심히 쓰고있는 글대충 리뷰를 열심히 쓰고있는 글대충 리뷰를 열심히 쓰고있는 글대충 리뷰를 열심히 쓰고있는 글대충 리뷰를 열심히 쓰고있는 글대충 리뷰를 열심히 쓰고있는 글대충 리뷰를 열심히 쓰고있는 글대충 리뷰를 열심히 쓰고있는 글대충 리뷰를 열심히 쓰고있는 글</p>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="top-review">
+                        <div class="review-thumbnail">
+                            <img src="images/6.bmp" alt="썸네일">
+                        </div>
+                        <div class="review-content">
+                            <div class="review-title">
+                                <h2>이것은 리뷰다!</h2>
+                            </div>
+                            <div class="review-in-content">
+                                <p>대충 리뷰를 열심히 쓰고있는 글대충 리뷰를 열심히 쓰고있는 글대충 리뷰를 열심히 쓰고있는 글대충 리뷰를 열심히 쓰고있는 글대충 리뷰를 열심히 쓰고있는 글대충 리뷰를 열심히 쓰고있는 글대충 리뷰를 열심히 쓰고있는 글대충 리뷰를 열심히 쓰고있는 글대충 리뷰를 열심히 쓰고있는 글대충 리뷰를 열심히 쓰고있는 글대충 리뷰를 열심히 쓰고있는 글대충 리뷰를 열심히 쓰고있는 글대충 리뷰를 열심히 쓰고있는 글대충 리뷰를 열심히 쓰고있는 글대충 리뷰를 열심히 쓰고있는 글</p>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="top-review">
+                        <div class="review-thumbnail">
+                            <img src="images/6.bmp" alt="썸네일">
+                        </div>
+                        <div class="review-content">
+                            <div class="review-title">
+                                <h2>이것은 리뷰다!</h2>
+                            </div>
+                            <div class="review-in-content">
+                                <p>대충 리뷰를 열심히 쓰고있는 글대충 리뷰를 열심히 쓰고있는 글대충 리뷰를 열심히 쓰고있는 글대충 리뷰를 열심히 쓰고있는 글대충 리뷰를 열심히 쓰고있는 글대충 리뷰를 열심히 쓰고있는 글대충 리뷰를 열심히 쓰고있는 글대충 리뷰를 열심히 쓰고있는 글대충 리뷰를 열심히 쓰고있는 글대충 리뷰를 열심히 쓰고있는 글대충 리뷰를 열심히 쓰고있는 글대충 리뷰를 열심히 쓰고있는 글대충 리뷰를 열심히 쓰고있는 글대충 리뷰를 열심히 쓰고있는 글대충 리뷰를 열심히 쓰고있는 글</p>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="top-review">
+                        <div class="review-thumbnail">
+                            <img src="images/6.bmp" alt="썸네일">
+                        </div>
+                        <div class="review-content">
+                            <div class="review-title">
+                                <h2>이것은 리뷰다!</h2>
+                            </div>
+                            <div class="review-in-content">
+                                <p>대충 리뷰를 열심히 쓰고있는 글대충 리뷰를 열심히 쓰고있는 글대충 리뷰를 열심히 쓰고있는 글대충 리뷰를 열심히 쓰고있는 글대충 리뷰를 열심히 쓰고있는 글대충 리뷰를 열심히 쓰고있는 글대충 리뷰를 열심히 쓰고있는 글대충 리뷰를 열심히 쓰고있는 글대충 리뷰를 열심히 쓰고있는 글대충 리뷰를 열심히 쓰고있는 글대충 리뷰를 열심히 쓰고있는 글대충 리뷰를 열심히 쓰고있는 글대충 리뷰를 열심히 쓰고있는 글대충 리뷰를 열심히 쓰고있는 글대충 리뷰를 열심히 쓰고있는 글</p>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="top-review">
+                        <div class="review-thumbnail">
+                            <img src="images/6.bmp" alt="썸네일">
+                        </div>
+                        <div class="review-content">
+                            <div class="review-title">
+                                <h2>이것은 리뷰다!</h2>
+                            </div>
+                            <div class="review-in-content">
+                                <p>대충 리뷰를 열심히 쓰고있는 글대충 리뷰를 열심히 쓰고있는 글대충 리뷰를 열심히 쓰고있는 글대충 리뷰를 열심히 쓰고있는 글대충 리뷰를 열심히 쓰고있는 글대충 리뷰를 열심히 쓰고있는 글대충 리뷰를 열심히 쓰고있는 글대충 리뷰를 열심히 쓰고있는 글대충 리뷰를 열심히 쓰고있는 글대충 리뷰를 열심히 쓰고있는 글대충 리뷰를 열심히 쓰고있는 글대충 리뷰를 열심히 쓰고있는 글대충 리뷰를 열심히 쓰고있는 글대충 리뷰를 열심히 쓰고있는 글대충 리뷰를 열심히 쓰고있는 글</p>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="top-review">
+                        <div class="review-thumbnail">
+                            <img src="images/3.jpg" alt="썸네일">
+                        </div>
+                        <div class="review-content">
+                            <div class="review-title">
+                                <h2>이것은 리뷰다!</h2>
+                            </div>
+                            <div class="review-in-content">
+                                <p>대충 리뷰를 열심히 쓰고있는 글대충 리뷰를 열심히 쓰고있는 글대충 리뷰를 열심히 쓰고있는 글대충 리뷰를 열심히 쓰고있는 글대충 리뷰를 열심히 쓰고있는 글대충 리뷰를 열심히 쓰고있는 글대충 리뷰를 열심히 쓰고있는 글대충 리뷰를 열심히 쓰고있는 글대충 리뷰를 열심히 쓰고있는 글대충 리뷰를 열심히 쓰고있는 글대충 리뷰를 열심히 쓰고있는 글대충 리뷰를 열심히 쓰고있는 글대충 리뷰를 열심히 쓰고있는 글대충 리뷰를 열심히 쓰고있는 글대충 리뷰를 열심히 쓰고있는 글</p>
+                            </div>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="lately-review">
+            <div class="review-head">
+                <span>최신 리뷰</span>
+            </div>
+        </div>
+    </section>
 @endsection
